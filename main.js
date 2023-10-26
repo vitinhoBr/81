fd44013412308cd1277a80926d945f6562318ecf
vitinhoBr <144251848+vitinhoBr@users.noreply.github.com>
2023-10-25 21:34:32 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,33 +1,10 @@
+canvas=
+document.getElementById("myCanvas");
+ctx=canvas.getContext("2d");
 
-menuListaArray=["Pizza Vegetariana",
-                   "Pizza de Frango",
-                   "Pizza Portuguesa",
-                   "Pizza Quatro Queijos",
-                   "Pizza Calabresa",
-                   "Pizza Extravaganza"];
-function getMenu(){
-    var htmldata=" ";
-    menuListaArray.sort();
-    for(var i=0;i<menuListaArray.length;i++){
-        htmldata=htmldata+ menuListaArray[i]+'<br>'
-    }
-    document.getElementById("displayMenu").innerHTML=htmldata;
-}
 
-function addItem (){
-    var htmldata;
-    var imgtags=' <img id="im1" src="images/pizzaImg.png" />'
-    var item=document.getElementById("addItem").value;
-    menuListaArray.sort();
-    htmldata=""
-    for(var i=0;i<menuListaArray.length;i++){
-        htmldata=htmldata+imgtags+menuListaArray[i]+'<br>';
-    }
-    document.getElementById("displayAddedMenu").innerHTML=htmldata
-
-}
-function addTop(){
-    var item=document.getElementById("addItem").value;
-    menuListaArray.push(Item);
-        addItem();
-}
+ctx.beginPath();
+ctx.strokeStyle="red";
+ctx.lineWidth=1;
+ctx.arc(250,210,40,0,2*Math.PI);
+ctx.stroke();
